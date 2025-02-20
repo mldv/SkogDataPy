07c5c83ca140e9ab32b87b1a44c438acf80b273b
Marco L. DELLA VEDOVA <marco.dellavedova@gmail.com>
2025-02-20 15:24:38 +0100

    Clarification on password being stored plain text

--- tests/test_connections.py
+++ tests/test_connections.py
@@ -8,8 +8,8 @@ def test_connection_sgd():
     host = "ftpsks.skogsstyrelsen.se"
     port = 990
     user = "SGD"
-    passwd = "0N!nd=I9EJ"
-    _timeout_=30
+    passwd = "0N!nd=I9EJ"  # Note: this password is publicly available at https://www.skogsstyrelsen.se/sjalvservice/karttjanster/geodatatjanster/ftp/
+    _timeout_ = 30
     with ImplicitFTP_TLS() as ftps:
         ftps.set_debuglevel(0)
         ftps.connect(host=host, port=port, timeout=_timeout_)
